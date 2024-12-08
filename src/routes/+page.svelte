ad18ac3bbcb36b15a20e019b2629c0801c895eea
batcsg1 <batcsg1@student.op.ac.nz>
2024-12-08 21:37:43 +1300

    Added content into info section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
-<section>
+<section id="banner">
     <h2>No Hassle IT Support, So you can keep doing what you do Best!</h2>
+</section>
+<section>
+    <h3>IT for business doesn't need to be confusing but it does need to be safe.</h3>
+    <p>Talk to us about how we can help you to secure your IT systems, protecting your clients information.
+        Easy to manage IT solutions that you understand, no geek speak, no bull, just simple robust systems to make your business perform in
+        this technically challenging world.       
+        Contact us to see you your IT systems can work better for you.
+    </p>
+    <iframe width="200" height="200" 
+        src="https://www.youtube.com/embed/4F1ZaZ87Mnc"
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+    </iframe>
 </section>
 
 <style>
-    section{
+    #banner{
         background-color: yellow;
         display: flex;
         justify-content: center;
