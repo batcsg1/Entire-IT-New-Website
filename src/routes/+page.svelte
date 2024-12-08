97b2d16bbdfa4b1461a05115021660d585d903af
batcsg1 <batcsg1@student.op.ac.nz>
2024-12-08 21:33:33 +1300

    Added test section for image banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,13 @@
 <section>
-    <h2>Test</h2>
-</section>
-<section>
-    <h2>Test</h2>
-</section>
-<section>
-    <h2>Test</h2>
+    <h2>No Hassle IT Support, So you can keep doing what you do Best!</h2>
 </section>
 
 <style>
     section{
         background-color: yellow;
+        display: flex;
+        justify-content: center;
+        padding: 3em;
     }
 </style>
 
