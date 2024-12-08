065f4256a951190e431e59cfca3a6ba49eac7af1
batcsg1 <batcsg1@student.op.ac.nz>
2024-12-08 21:24:57 +1300

    Added animation for header items and more styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -17,23 +17,67 @@
 <style>
     header{
         padding: 1em;
-        background-color: greenyellow;
         align-items: center;
-    }
-    img{
-        border: 3px solid red;
+        justify-content: center;
+        gap: 5em;
+        color:rgb(6, 4, 43);
     }
     header, ul{
         display: flex;
     }
     ul{
-        background-color: red;
         list-style: none;
         padding: 1em;
-        gap: 0.5em;
+        gap: 1.5em;
     }
     ul li{
         padding: 0.5em;
         background-color: white;
+        border-radius: 0.4em;
+    }
+    /*Hover effect for nav items*/
+    ul li:hover{
+        background-color:rgb(6, 4, 43);
+        color: white;
+    }
+
+    /* Keyframes for the drop effect */
+    @keyframes dropIn {
+        0% {
+            opacity: 0;
+            transform: translateY(-20px);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    /* Apply the dropIn animation to each item with a staggered delay */
+    img{
+        animation: dropIn 0.5s ease forwards;
+        opacity: 0;  
+    }    
+    ul li {
+        animation: dropIn 0.5s ease forwards;
+        opacity: 0;
+    }
+    ul li:nth-child(1) {
+        animation-delay: 0.1s;
+    }
+    ul li:nth-child(2) {
+        animation-delay: 0.2s;
+    }
+    ul li:nth-child(3) {
+        animation-delay: 0.3s;
+    }
+    ul li:nth-child(4) {
+        animation-delay: 0.4s;
+    }
+    ul li:nth-child(5) {
+        animation-delay: 0.5s;
+    }
+    ul li:nth-child(6) {
+        animation-delay: 0.6s;
     }
 </style>
