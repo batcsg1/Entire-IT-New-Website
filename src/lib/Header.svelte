87ef3e08ce7258c1efbddc00a52725d1eb9dfcc1
batcsg1 <batcsg1@student.op.ac.nz>
2024-12-08 21:17:46 +1300

    Added logo as header and nav menu

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,39 @@
 <script>
-
+    import Logo from '$lib/Images/logo.png'
 </script>
 
 <header>
-    <h1>Entire IT</h1>
+    <img src={Logo} alt="Logo" height=80>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>Status</li>
+        <li>Help</li>
+    </ul>  
 </header>
 
 <style>
-
+    header{
+        padding: 1em;
+        background-color: greenyellow;
+        align-items: center;
+    }
+    img{
+        border: 3px solid red;
+    }
+    header, ul{
+        display: flex;
+    }
+    ul{
+        background-color: red;
+        list-style: none;
+        padding: 1em;
+        gap: 0.5em;
+    }
+    ul li{
+        padding: 0.5em;
+        background-color: white;
+    }
 </style>
