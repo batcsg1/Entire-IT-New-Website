7694885e7edf8ca0e0f92278d7fcda1c3afc6f3c
batcsg1 <batcsg1@student.op.ac.nz>
2024-12-08 21:44:16 +1300

    Changed alignment axis of info contain

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <section id="banner">
     <h2>No Hassle IT Support, So you can keep doing what you do Best!</h2>
 </section>
-<section>
-    <h3>IT for business doesn't need to be confusing but it does need to be safe.</h3>
-    <p>Talk to us about how we can help you to secure your IT systems, protecting your clients information.
+<section id="info">
+    <section>
+        <h3>IT for business doesn't need to be confusing but it does need to be safe.</h3>
+        <p>Talk to us about how we can help you to secure your IT systems, protecting your clients information.
         Easy to manage IT solutions that you understand, no geek speak, no bull, just simple robust systems to make your business perform in
         this technically challenging world.       
         Contact us to see you your IT systems can work better for you.
-    </p>
-    <iframe width="200" height="200" 
+        </p>
+    </section>
+    <iframe width="400" height="300" 
         src="https://www.youtube.com/embed/4F1ZaZ87Mnc"
         title="YouTube video player" 
         frameborder="0" 
@@ -23,6 +25,23 @@
         display: flex;
         justify-content: center;
         padding: 3em;
+    }
+    #info{
+        display: flex;
+        flex-direction: column;
+        padding: 4em;
+        align-items: center;
+        gap: 3em;
+    }
+    #info section{
+        background-color: white;
+        padding: 1em;
+        min-width: 30em;
+        text-align: center;
+    }
+    h3{
+        text-transform: uppercase;
+        font-size: x-large;
     }
 </style>
 
